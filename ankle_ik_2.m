f80540df44f60806f1cac1b9e24210787f6df714
rocketman123456 <759094438@qq.com>
2023-11-23 22:05:41 +0800

    update calc

--- ankle_ik_2.m
+++ ankle_ik_2.m
@@ -19,34 +19,71 @@ dt = 1e-6;
 JF = [tMLx / dt, tMLy / dt; tMRx / dt, tMRy / dt];
 J = pinv(JF);
 
+start_x = -10;
+step_x = 0.2;
+end_x = 10;
+
+start_y = -20;
+step_y = 0.2;
+end_y = 20;
+
+[tx_mat, ty_mat] = meshgrid(start_x:step_x:end_x, start_y:step_y:end_y);
+t1_mat = [];
+t2_mat = [];
+i = 1;
+hold on
+for tx = start_x:step_x:end_x
+    t1_list = [];
+    t2_list = [];
+    for ty = start_y:step_y:end_y
+        [t1, t2] = ankle_ik_(L1, d, h1, h2, tx, ty);
+        t1_list = [t1_list, t1];
+        t2_list = [t2_list, t2];
+    end
+    t1_mat(:,i) = t1_list;
+    t2_mat(:,i) = t2_list;
+    i = i + 1;
+end
+
+% mesh(tx_mat, ty_mat, t1_mat);
+mesh(tx_mat, ty_mat, t2_mat);
+
 function [tML, tMR] = ankle_ik_(L1, d, h1, h2, tx, ty)
-
-cx = cosd(tx);
-cy = cosd(ty);
-sx = sind(tx);
-sy = sind(ty);
-
-AL = - L1 * L1 * cy + L1 * d * sx * sy;
-BL = -L1 * L1 * sy + L1 * h1 - L1 * d * sx * cy;
-CL = -(L1 * L1 + d * d - d * d *cx - L1 * h1 * sy - d * h1 * sx * cy);
-
-LenL = sqrt(AL * AL + BL * BL);
-
-AR = - L1 * L1 * cy - L1 * d * sx * sy;
-BR = -L1 * L1 * sy + L1 * h2 + L1 * d * sx * cy;
-CR = -(L1 * L1 + d * d - d * d *cx - L1 * h2 * sy + d * h2 * sx * cy);
-
-LenR = sqrt(AR * AR + BR * BR);
-
-tML_1 = asind(CL / LenL) - asind(AL / LenL);
-tML_2 = asind(CL / LenL) + acosd(BL / LenL);
-assert(tML_1 - tML_2 < 1e-6)
-
-tMR_1 = asind(CR / LenR) - asind(AR / LenR);
-tMR_2 = asind(CR / LenR) + acosd(BR / LenR);
-assert(tMR_1 - tMR_2 < 1e-6)
-
-tML = tML_1;
-tMR = tMR_1;
-
+    cx = cosd(tx);
+    cy = cosd(ty);
+    sx = sind(tx);
+    sy = sind(ty);
+    
+    AL = - L1 * L1 * cy + L1 * d * sx * sy;
+    BL = -L1 * L1 * sy + L1 * h1 - L1 * d * sx * cy;
+    CL = -(L1 * L1 + d * d - d * d *cx - L1 * h1 * sy - d * h1 * sx * cy);
+    
+    LenL = sqrt(AL * AL + BL * BL);
+    
+    AR = - L1 * L1 * cy - L1 * d * sx * sy;
+    BR = -L1 * L1 * sy + L1 * h2 + L1 * d * sx * cy;
+    CR = -(L1 * L1 + d * d - d * d *cx - L1 * h2 * sy + d * h2 * sx * cy);
+    
+    LenR = sqrt(AR * AR + BR * BR);
+    
+    if(LenL < abs(CL))
+        tML_1 = 0;
+        tML_2 = 0;
+    else
+        tML_1 = asind(CL / LenL) - asind(AL / LenL);
+        tML_2 = asind(CL / LenL) + acosd(BL / LenL);
+    end
+    assert(tML_1 - tML_2 < 1e-6)
+    
+    if(LenR < abs(CR))
+        tMR_1 = 0;
+        tMR_2 = 0;
+    else
+        tMR_1 = asind(CR / LenR) - asind(AR / LenR);
+        tMR_2 = asind(CR / LenR) + acosd(BR / LenR);
+    end
+    assert(tMR_1 - tMR_2 < 1e-6)
+    
+    tML = tML_1;
+    tMR = tMR_1;
 end
--- humanoid_leg_l_define.m
+++ humanoid_leg_l_define.m
@@ -1,28 +1,35 @@
 syms dL L1 L2 L3 real;
+syms t1 t2 t3 t4 t5 t6 real;
 
 w1 = [1, 0, 0]';
 p1 = [0, dL, 0]';
-S1 = build_screw(w1, p1)
+S1 = build_screw(w1, p1);
 
 w2 = [0, 0, 1]';
 p2 = [0, dL, 0]';
-S2 = build_screw(w2, p2)
+S2 = build_screw(w2, p2);
 
 w3 = [0, 1, 0]';
 p3 = [0, dL, 0]';
-S3 = build_screw(w3, p3)
+S3 = build_screw(w3, p3);
 
 w4 = [0, 1, 0]';
 p4 = [0, dL, -L1]';
-S4 = build_screw(w4, p4)
+S4 = build_screw(w4, p4);
 
 w5 = [0, 1, 0]';
 p5 = [0, dL, -L1-L2]';
-S5 = build_screw(w5, p5)
+S5 = build_screw(w5, p5);
 
 w6 = [1, 0, 0]';
 p6 = [0, dL, -L1-L2]';
-S6 = build_screw(w6, p6)
+S6 = build_screw(w6, p6);
+
+M = [1, 0, 0, 0;  0, 1, 0, -dL; 0, 0, 1, -L1-L2-L3; 0, 0, 0, 1];
+SList = [S1, S2, S3, S4, S5, S6];
+thetalist = [t1, t2, t3, t4, t5, t6];
+
+T = FKinSpace(M, SList, thetalist)
 
 function S = build_screw(w, p)
     v = - cross(w, p);
